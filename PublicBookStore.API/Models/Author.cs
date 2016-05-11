91c19bebc0a8475b1fccd3d52b71187d314eae27
Mecit Semerci <mecit.semerci@gmail.com>
2016-05-11 16:29:50 +0300

    virtual Books

--- PublicBookStore.API/Models/Author.cs
+++ PublicBookStore.API/Models/Author.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PublicBookStore.API.Models
@@ -7,5 +8,7 @@ namespace PublicBookStore.API.Models
     {
         public int AuthorId { get; set; }
         public string Name { get; set; }
+
+        public virtual List<Book> Books { get; set; }
     }
 }
