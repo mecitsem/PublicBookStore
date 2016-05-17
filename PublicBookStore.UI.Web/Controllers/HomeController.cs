a19374656af05fba92777d89cde9dda47006ca4b
Mecit Semerci <mecit.semerci@gmail.com>
2016-05-17 14:50:39 +0300

    Added books filter by genre

--- PublicBookStore.UI.Web/Controllers/HomeController.cs
+++ PublicBookStore.UI.Web/Controllers/HomeController.cs
@@ -18,23 +18,7 @@ namespace PublicBookStore.UI.Web.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-
-      
+   
 
     }
 }
